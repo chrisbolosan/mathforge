934dbd3f5f7eb0d9bd9eecc2c155c5a56fa8742f
chrisbolosan <cbolosan@gmail.com>
2025-02-11 16:57:57 -0600

    draw tank and water with logic

--- cylinder_vizualization.py
+++ cylinder_vizualization.py
@@ -1,6 +1,9 @@
 import pygame
 import sympy as sp
 
+
+pygame.init()
+
 WIDTH, HEIGHT = 600, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Vizualization of Cyclinder Water Pump work")
@@ -26,5 +29,32 @@ force = weight_density * volume_element
 distance =y
 work_integral = sp.integrate(force*distance, (y, 10, 12)) #bounds of integration
 work_done = work_integral.simplify()
-pygame.init()
 
+font = pygame.font.Font(None, 30)
+
+
+running = True
+while running:
+    screen.fill(WHITE)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.draw.rect(screen, BLACK, (tank_x, tank_y, tank_width, tank_height), 2)
+#WATER
+pygame.draw.rect(screen, BLUE, (tank_x, tank_y + (tank_height - water_level), tank_width, water_level))
+
+#calculsations display
+work_text = font.render(f"Work Done: {work_done} N-m", True, BLACK)
+screen.blit(work_text, (50, 50))
+
+
+if water_level < max_water_height:
+    water_level += filling_speed
+    
+pygame.display.flip()
+pygame.time.delay(100)
+
+
+pygame.quit()
