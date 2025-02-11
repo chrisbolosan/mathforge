eee145f2b9575c8f8880be9c05f92dff70b717c7
chrisbolosan <cbolosan@gmail.com>
2025-02-11 09:58:27 -0600

    begin vizualization cyclinder work

---
+++ cylinder_vizualization.py
@@ -0,0 +1,8 @@
+import pygame
+import sympy as sp
+
+WIDTH, HEIGHT = 600, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Vizualization of Cyclinder Water Pump work")
+pygame.init()
+
