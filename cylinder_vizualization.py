f2ab9f4610a70007e877ebc14e12c6e582c0fa85
chrisbolosan <cbolosan@gmail.com>
2025-02-11 19:49:59 -0600

    integrate streamlit

--- cylinder_vizualization.py
+++ cylinder_vizualization.py
@@ -1,12 +1,13 @@
 import pygame
 import sympy as sp
-
+import streamlit as st
+import numpy as np
+from PIL import Image
 
 pygame.init()
 
 WIDTH, HEIGHT = 600, 500
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Vizualization of Cyclinder Water Pump work")
+screen = pygame.Surface((WIDTH, HEIGHT))
 
 WHITE = (255, 255, 255)
 BLUE = (0, 100, 255)
@@ -30,31 +31,32 @@ distance =y
 work_integral = sp.integrate(force*distance, (y, 10, 12)) #bounds of integration
 work_done = work_integral.simplify()
 
-font = pygame.font.Font(None, 30)
 
+if water_level < max_water_height:
+    water_level += filling_speed
+
+st.title("Water Tank Work Calculation")
+st.write("Simulating water filling in a tank while computing work done.")
 
 running = True
-while running:
+def draw_ui():
     screen.fill(WHITE)
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
     pygame.draw.rect(screen, BLACK, (tank_x, tank_y, tank_width, tank_height), 2)
     #WATER
     pygame.draw.rect(screen, BLUE, (tank_x, tank_y + (tank_height - water_level), tank_width, water_level))
 
+    font = pygame.font.Font(None, 30)
+    
     #calculsations display
     work_text = font.render(f"Work Done: {work_done} N-m", True, BLACK)
     screen.blit(work_text, (50, 50))
-
-
-    if water_level < max_water_height:
-        water_level += filling_speed
     
-    pygame.display.flip()
-    pygame.time.delay(100)
+    return screen
 
-
-pygame.quit()
+draw_ui()
+frame_array = pygame.surfarray.array3d(screen)  
+frame_array = np.rot90(frame_array,-1) 
+frame_array = np.fliplr(frame_array)
+frame_image = Image.fromarray(frame_array) 
+st.image( frame_image,caption="Water Tank Simulation", use_container_width=True)
