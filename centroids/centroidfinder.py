e0eba978706735f5d2c09b7759fdd7693aa49741
chrisbolosan <cbolosan@gmail.com>
2025-03-03 21:17:41 -0600

    centroid single orientation WIP

--- centroids/centroidfinder.py
+++ centroids/centroidfinder.py
@@ -2,23 +2,33 @@
 #[T] Lens: y=x^2 abd y=x
 
 import sympy as sp
+from sympy import *
 
 x=sp.Symbol('x')
 
-y=x**2
-y=x
-
 # functions under the curve. If horizontal bar, use y=f(x), if vertical bar, use x=f(y)
 f1 =x
-f2 = x**2
+f2 =x**2
 
 #intersection points against x-axis
-INTERSECTION_POINTS = sp.solve(f1-f2,x)
-
-x0, x1 = INTERSECTION_POINTS
+x_intersections = sp.solve(f1-f2,x)
+x0, x1 = x_intersections
 
 # total area
-
 M = sp.integrate(f1-f2, (x,x0,x1))
 
 #moments
+My = sp.integrate(x*(f1-f2),(x,x0,x1))
+Mx = sp.integrate(((f1+f2)/2)*(f1-f2),(x,x0,x1))
+
+x_bar = My/M
+y_bar = Mx/M
+
+#results
+print("\n===== Results =====")
+print(f"Intersection Points: {x_intersections}")
+print(f"Moment of Y-Axis: {My}")
+print(f"Moment of X-Axis: {Mx}")
+print(f"x̄: {x_bar}")
+print(f"ȳ: {y_bar}")
+print(f"Centroid is: ({x_bar} , {y_bar})")
